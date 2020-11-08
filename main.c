ad882bd8c2290e808f1b380d19bc6bd6011bd422
Hugo Andrade <hugotsandrade@gmail.com>
2020-11-08 20:19:44 +0000

    Inclusão do exercício 2

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#define ex1
+#define ex2
+
 #include <stdio.h>
 
 #ifdef ex1
@@ -8,12 +9,12 @@ Exiba também a posição e o respectivo par. Informe ao usuário caso não tenh
 
 int vetA[6];
 
-int funçãovetA(){
+int funcaovetA(){
 
   int x;
   int contpar = 0;
 
-  printf("Digite um número qualquer, até preencher um vetor de 6 posições.\n");
+  printf("Ex.1: Digite um número qualquer, até preencher um vetor de 6 posições.\n");
 
   for(x = 1; x <= 6; x++){
     scanf("%d", &vetA[x]);
@@ -35,8 +36,52 @@ int funçãovetA(){
 return 0;
 }
 int main() {
-  funçãovetA();
+  funcaovetA();
 }
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef ex2
+
+/*2.	Ler um conjunto de 6 números reais, armazenando-o no vetor A. Calcular o quadrado das componentes deste vetor, 
+armazenando o resultado no vetor B. Exiba os dois vetores. 
+Exemplo: Suponha que você tenha digitado 2 na posição zero do vetor A no vetor B na mesma posição será o valor de A[0] ao quadrado. 
+A[0]=2 e B[0]= 4  e assim sucessivamente. 
+*/
+
+float vetA[6];
+float vetB[6];
+
+int funcaovetAeB(){
+  int x;
+
+  printf("Ex.2: Digite 6 números reais até preencher um vetor de 6 posições.\n");
+  printf("O vetor B irá armazenar o número ao quadrado digitado no vetor A, de acordo com as posições.\n");
+  printf("Exemplo: A[0]=2 e B[0]= 4  e assim sucessivamente.\n");
+
+  for(x = 0; x < 6; x++){
+    scanf("%f", &vetA[x]);
+    vetB[x] = vetA[x] * vetA[x];
+  }
+  for(x = 0; x < 6; x++){
+  printf("\nA[%d] = %.2f e B[%d] = %.2f;", x, vetA[x], x, vetB[x]);
+  }
+return 0;
+
+}
+
+int main() {
+  funcaovetAeB();
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef ex3
 
 #endif
 
